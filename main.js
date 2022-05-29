a43de136f471d2b1479fbe777153e8e379a336f4
AllenY687 <allenyang687@gmail.com>
2022-05-29 11:05:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function start() {
     const videoCanvas = document.createElement("canvas");
     videoCanvas.width = "640";
     videoCanvas.height = "480";
-    videoCanvas.style = "background: url('https://www.looper.com/img/gallery/things-only-adults-notice-in-shrek/intro-1573597941.jpg')";
+    videoCanvas.style = "background: url('https://cdna.lystit.com/520/650/n/photos/lvr/b46559eb/gucci-Black-Logo-Print-Cotton-Jersey-T-shirt.jpeg')";
 
     const div = document.getElementById("banner");
     div.appendChild(videoCanvas);
